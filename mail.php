f2ae2a13e780c48c3bf55ebd537883b6b236b245
Jim Brothers <8008538+jimkel97@users.noreply.github.com>
2020-01-04 14:55:45 -0500

    contact

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@ $message = $_POST['message'];
 
 $formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
 
-$recipient = "jim.brothers@live.COM";
+$recipient = "YOUREMAIL@HERE.COM";
 
 $subject = "Contact Form";
 
@@ -26,6 +26,6 @@ $mailheader = "From: $email \r\n";
 
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 
-echo "Thank You!" . " -" . "<a href='index.htm' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 
 ?>
